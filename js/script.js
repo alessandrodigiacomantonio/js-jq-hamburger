c32941eeaad2625c48559650243566bd79b4b3d0
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-16 15:35:39 +0100

    Js: aggiunti attivatori hamburger-menu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var hambMenuShow = false;
+$('.header-right .fas.fa-bars').on('click', function() {
+  if (!hambMenuShow) $('.hamburger-menu').show();
+  else $('.hamburger-menu').hide();
+}
+);
+$('.fas.fa-times').on('click', function() {
+  $('.hamburger-menu').hide();
+}
+);
