0ed033b5317360f0975e09a5198c3593ad030f85
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-16 16:28:54 +0100

    Accorpato codice

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-var hambMenuShow = false;
-$('.header-right .fas.fa-bars').on('click', function() {
-  if (!hambMenuShow) $('.hamburger-menu').show();
-  else $('.hamburger-menu').hide();
-}
-);
+$('.fas.fa-bars').on('click', function() {
+  $('.hamburger-menu').show();
+});
 $('.fas.fa-times').on('click', function() {
   $('.hamburger-menu').hide();
-}
-);
+});
